1743c795a826522b02ecfaef23ff06deac0842aa
Bramdon <bramdonbarajas2015@gmail.com>
2025-05-28 18:59:38 -0500

    cambio 3

--- routes/users.js
+++ routes/users.js
@@ -13,7 +13,7 @@ router.get('/', (_req, res) => {
 // Agregar un usuario
 router.post('/', (req, res) => {
   const { Name, Last_Name, Phone, Direccion, Email, Contraseña } = req.body;
-  db.query('INSERT INTO Users (Name, Last_Name, Phone, Direccion, Email, Contraseña) VALUES (?, ?, ?, ?, ?, ?)', [Name, Last_Name, Phone, Direccion, Email, Contraseña], (err, result) => {
+  db.query('INSERT INTO users (Name, Last_Name, Phone, Direccion, Email, Contraseña) VALUES (?, ?, ?, ?, ?, ?)', [Name, Last_Name, Phone, Direccion, Email, Contraseña], (err, result) => {
     if (err) return res.status(500).json({ error: err });
     res.json({ id: result.insertId, Name, Last_name, Phone, Direccion, Email });
   });
